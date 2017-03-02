2d33a8a07addfc896f8cedec887795855acb5b87
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-03-01 19:43:42 -0600

    modified api url.

--- index.js
+++ index.js
@@ -2,24 +2,40 @@
 
     //Global variables
     var httpRequest;
-    document.getElementById("newQuote").addEventListener("click", newQuote);
 
-    // New Quote Function handles the api request
-    function newQuote() {
+    // Add api request as an event listener
+    document.getElementById("newQuote").addEventListener("click", handleApi);
+
+    //***********************************************
+    // handleApi Function handles the api request   *
+    // for forismatic to send us a random quote     *
+    //***********************************************
+    function handleApi() {
         httpRequest = new XMLHttpRequest();
+
         // Local Variables
         let method = "GET",
-            url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en";
+            url = "http://quotes.stormconsultancy.co.uk/random.json";
+        //http://quotes.stormconsultancy.co.uk/random.json
 
+        //call (open, readystate, and send) methods
         httpRequest.open(method, url, true);
-        httpRequest.onreadystatechange = requestStatus;
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+                console.log(httpRequest.responseText);
+            }
+        };
         httpRequest.send();
     }
 
-    function requestStatus() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
-            console.log(httpRequest.responseText);
-        }
-    }
+
+    //**************************************************************
+    //* requestStatus function handles the actions to be perfomed  *
+    //* when the document is read                                  *
+    //**************************************************************
+
+
+
+
 
 })();
